bc83ef8dfcf3ebf943bd59a38d20f875955e72dd
Zende-e <68330569+Zende-e@users.noreply.github.com>
2022-06-13 13:09:25 +0300

    Add files via upload

---
+++ task 6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesCount=52;
+            int picturesInRow = 3;
+
+            Console.WriteLine("Полностью заполненных рядов: {0} Осталось картинок: {1}", picturesCount/picturesInRow, picturesCount % picturesInRow);
+
+
+        }
+    }
+}
