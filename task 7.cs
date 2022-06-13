ef032013d7ea1ac10d751ba6d0e4d015eced73ec
Zende-e <68330569+Zende-e@users.noreply.github.com>
2022-06-13 13:41:05 +0300

    Add files via upload

---
+++ task 7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldInBackPack;
+            int crystalsToBuy;
+            int crystalExchangeRate;
+            int crystalsInBackPack = 0;
+
+            Console.Write("Cколько у вас золота: ");
+            goldInBackPack = Convert.ToInt32(Console.ReadLine());
+
+            while (true)
+            {
+                Console.Write("Cколько кристалов купить: ");
+                crystalsToBuy = Convert.ToInt32(Console.ReadLine());
+
+                Random random = new Random();
+                crystalExchangeRate = random.Next(1, 100);
+                Console.WriteLine("Курс кристала  {0} ", crystalExchangeRate);
+
+
+                goldInBackPack -= crystalsToBuy * crystalExchangeRate;
+                crystalsInBackPack += crystalsToBuy;
+
+                Console.WriteLine("Кристалов в рюкзаке  {0} ", crystalsInBackPack);
+                Console.WriteLine("Золота в Рюкзаке {0}", goldInBackPack);
+            }
+        }
+    }
+}
