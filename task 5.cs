2fafac2b73d65026261dd6b960b88afdb8a1dbbc
Zende-e <68330569+Zende-e@users.noreply.github.com>
2022-06-13 13:01:34 +0300

    Add files via upload

---
+++ task 5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "Иванов";
+            string userSurname = "Олег";
+
+            Console.WriteLine("Имя: "+userName+ "\n" + "Фамилия: " + userSurname);
+
+            string temporaryVariable = userName;
+            userName = userSurname;
+            userSurname = temporaryVariable;
+
+            Console.WriteLine("Имя: " + userName + "\n" + "Фамилия: " + userSurname);
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
