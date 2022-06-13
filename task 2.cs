1c64f75c2eb2350a6e986d256ba19ec5a118ae2f
Zende-e <68330569+Zende-e@users.noreply.github.com>
2022-06-13 12:43:03 +0300

    Add files via upload

---
+++ task 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldCount;
+            string playerName;
+            float playerResist;
+            bool isPlayerDead;
+            double armorWeight;
+            uint questNumber;
+            char playerInitials;
+            ushort playerHealth;
+            ulong skillPointsCount;
+            byte armorColorRed;
+        }
+    }
+}
