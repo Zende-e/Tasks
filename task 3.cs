fd8cea088b781dc5ad01bd873a44426f3eb6581c
Zende-e <68330569+Zende-e@users.noreply.github.com>
2022-06-13 12:45:15 +0300

    Add files via upload

---
+++ task 3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _132
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            string userAge = Console.ReadLine();
+            Console.WriteLine("Укажите свой знак зодиака");
+            string zodiacSign = Console.ReadLine();
+
+            Console.WriteLine("Вас зовут " + userName + ", вам " + userAge + " год, " + " по знаку зодиака вы " + zodiacSign);
+            Console.ReadKey();
+        }
+    }
+}
